955304fef9bd565d7f8244b4edfc9926452c280b
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-06-06 00:21:59 +0300

    Update intertitre.py

--- intertitre.py
+++ intertitre.py
@@ -3,17 +3,6 @@ import telebot
 from telebot import types
 import browse
 
-##import time
-##from time import sleep
-##from collections import namedtuple
-##import Edit_sheet
-##import httplib2
-##import argparse
-##from apiclient import discovery
-##from oauth2client import client
-##from oauth2client import tools
-##from oauth2client.file import Storage
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 bot = telebot.TeleBot(config['DEFAULT']['Token'])
@@ -25,16 +14,16 @@ commands = {  'start': 'Описание бота',
 @bot.message_handler(commands=['start'])
 def start(m):
     cid = m.chat.id
-    bot.send_message(cid, 'Привет, этот бот подгружает случайный титр с сайта http://intertitre.togdazine.ru/')
+    bot.send_message(cid, 'Привет, этот бот подгружает случайный титр с сайта http://intertitre.togdazine.ru/.')
 
 @bot.message_handler(commands=['help'])
 def help(m):
     cid = m.chat.id
-    help_text = 'Доступны команды: \n'
-    for key in commands:  
-        help_text += '/' + key + ': '
-        help_text += commands[key] + '\n'
-    bot.send_message(cid, help_text)  
+##    help_text = 'Доступны команды: \n'
+##    for key in commands:  
+##        help_text += '/' + key + ': '
+##        help_text += commands[key] + '\n'
+    bot.send_message(cid, 'Как это работает: после вызова бота введите любой символ в поле сообщения.')  
 
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
 def titre(query):
